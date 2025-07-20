7be30085ff111841ab87d89065fd694eb2248d6e
John <103416957+johnreycalesa@users.noreply.github.com>
2025-07-20 08:38:08 +0800

    Remove version2 directory and all associated files including package.json, configuration files, assets, and pages.

--- version2/src/app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function About() {
-  return (
-    <div>
-      <h1>About Us</h1>
-    </div>
-  );
-}
--- version2/src/app/contact/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function Contact() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-    </div>
-  );
-}
