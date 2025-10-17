63bf3978909b2207edcc49db1e1a8bb26a2c5d4e
John <103416957+johnreycalesa@users.noreply.github.com>
2025-10-18 05:34:48 +0800

    Update ContactPage to use ContactHeader and ContactAreas
    Replaces ContactHeaders with ContactHeader and ContactServices with ContactAreas in ContactPage.jsx to reflect updated component names and structure.

--- src/core/components/main components/ContactPage.jsx
+++ src/core/components/main components/ContactPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { ContactHeaders } from "../additional components/ContactHeader";
-import { ContactServices } from "../additional components/ContactServices";
+import { ContactHeader } from "../additional components/ContactHeader";
+import { ContactAreas } from "../additional components/ContactAreas";
 import { Contacts } from "../common/Contacts/Contacts";
 import { Footer } from "../common/Footer/Footer";
 
 export function ContactPage() {
   return (
     <div>
-      <ContactHeaders />
-      <ContactServices />
+      <ContactHeader />
+      <ContactAreas />
       <Contacts />
       <Footer />
     </div>
