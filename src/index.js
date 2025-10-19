3615083ea456883ba44378744af2900fbb093b2b
John <103416957+johnreycalesa@users.noreply.github.com>
2025-10-19 09:22:58 +0800

    Wrap app with HelmetProvider for head management
    Added HelmetProvider from react-helmet-async to the root render tree to enable dynamic management of the document head. This prepares the app for components that need to update page titles or meta tags.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import Nav from "./core/components/common/Navigation Bar/Nav";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Nav />
-  </BrowserRouter>
+  <HelmetProvider>
+    <BrowserRouter>
+      <Nav />
+    </BrowserRouter>
+  </HelmetProvider>
 );
